b2bc84b99a6962e87c1e1e9914e6de5c76e884f9
nandahibatullah <nandahibatullah@gmail.com>
2017-08-12 10:48:21 +1200

    Loaded basic webpage to node server

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs")
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render("index.html.ejs");
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
