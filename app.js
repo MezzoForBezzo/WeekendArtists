cdb28ac960ec8d35fe1a236f42b90d92afc35c23
Matt <matthew.lee@ecs.vuw.ac.nz>
2017-08-12 11:52:54 +1200

    Successfully connected to DigitalNZ API and got image URL

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var request = require('request-promise-native');
+
 var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs")
@@ -10,12 +12,13 @@ var databaseVersion = "v3";
 
 function getExampleImage(){
   // DO STUFF
-  var id = "5983a0ca1257573778000722";
+  var id = "36680895";
   var digitalNZUrl = "http://api.digitalnz.org/" +databaseVersion +"/records/" +id +"?api_key=" +digitalNZKey;
-  var request = request({url: digitalNZUrl, json: true});
-  console.log(request);
-  return request;
-}
+  return request({url: digitalNZUrl, json: true}).then(data => {
+    var image = data.record.large_thumbnail_url;
+    console.log(image);
+  });
+};
 
 app.get('/', function (req, res) {
   getExampleImage().then(res.render("index.html.ejs"));
